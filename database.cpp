5ca854e4f4279a7ade1c4d73dbf6c595300692d5
unknown <vooleksandrov22@codingburgas.bg>
2024-11-15 23:45:32 +0200

    Add destructor, excute methos to Database class

--- database.cpp
+++ database.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
+#include <mysql_driver.h>
+#include <mysql_connection.h>
+#include <cppconn/statement.h>
+//#include <cppconn/resultset.h>
 #include "database.h"
 
-DB::Database::Database(sql::Connection* connection) {
-   std::cout << "Create Database instance. Connection valid: " << connection->isValid();
-   this->connection = connection;
+
+DB::Database::Database(const std::string &url, const std::string &username, const std::string &password, const std::string &schema) {
+    sql::mysql::MySQL_Driver *driver = sql::mysql::get_mysql_driver_instance();
+    this->driver = driver;
+    std::cout << "Created driver instance\n";
+
+    //create Connection instance
+    this->connection =  driver->connect(url, username, password);
+
+
+    if (connection->isValid()) {
+        std::cout << "Connected to Azure MySQL database successfully!\n";
+    } else {
+        std::cerr << "Connection failed.\n";
+    }
+
+    connection->setSchema(schema);
+    std::cout << "Connection valid: " << connection->isValid() << "\n";
+
+
+    statement = connection->createStatement();
+    std::cout << "Create Statement instance\n";
 }
+
+
+
+
+
+
+
+DB::Database::~Database() {
+    connection->close();
+    delete connection;
+    delete driver;
+}
+
 
--- database.h
+++ database.h
@@ -6,17 +6,32 @@
 #define DB_H
 
 #include <iostream>
+#include <mysql_driver.h>
 #include <mysql_connection.h>
+#include <cppconn/statement.h>
+#include <cppconn/resultset.h>
 
 namespace DB {
+
     class Database {
     public:
-        Database(sql::Connection* connection);
-        sql::Connection* getConnection(){ return connection; };
+        Database(const std::string& url, const std::string& username, const std::string& password, const std::string& schema);
 
+        sql::ResultSet* execute(std::string &query) { return statement->executeQuery(sql::SQLString(query)); }
+
+
+
+        sql::Connection* getConnection(){ return connection; };
+        sql::mysql::MySQL_Driver* getDriver(){ return driver; };
+
+
+
+        ~Database();
 
     private:
+        sql::mysql::MySQL_Driver* driver;
         sql::Connection* connection;
+        sql::Statement* statement;
     };
 }
 #endif //DB_H
